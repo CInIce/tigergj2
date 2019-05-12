bec22e1f7b8f499d1f92e0f0909a22dd2f29cfbe
ltb <ltb@cin.ufpe.br>
2019-05-12 20:30:42 -0300

    initial commit

---
+++ Assets/CharMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D _rigidbody;
+    [SerializeField]float _speed;
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.collider.tag=="throw"){
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    
+
+        Vector2 input;
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+        
+        // Vector3 moveDirection = gameObject.transform.position - _origPos; 
+    //  if (moveDirection != Vector3.zero) 
+    //  {
+        if(input.x != 0 || input.y != 0){
+         float angle = Mathf.Atan2(input.y, input.x) * Mathf.Rad2Deg;
+         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+         }
+    //  }
+
+        _rigidbody.velocity = input * _speed * Time.deltaTime;
+        
+        // if (input.x != 0)
+        // {
+        //     _rigidbody.velocity = new Vector2(input.x, 0) * _speed * Time.deltaTime;
+        // }
+        
+        // else if (input.y != 0)
+        // {
+        //     _rigidbody.velocity = new Vector2(0, input.y) * _speed * Time.deltaTime;
+        // }
+        
+        
+        
+
+    }
+}
---
+++ Assets/Thrower.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrower : MonoBehaviour
+{
+    bool hasGrabbed;
+    RaycastHit2D hit;
+    [SerializeField] float distance = 2;
+    [SerializeField] float throwForce;
+    GameObject throwableObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.Z)){
+            if(!hasGrabbed){
+                Physics2D.queriesStartInColliders = false;
+                hit = Physics2D.Raycast(transform.position, transform.right,distance);
+                
+                
+                if(hit.collider!= null){
+                    hasGrabbed = true;
+                }
+            }else{
+                hasGrabbed = false;
+            }
+
+            
+            
+        }
+
+        if(hasGrabbed){
+                hit.collider.gameObject.transform.position = transform.GetChild(0).position;
+                throwableObject = hit.collider.gameObject;
+        }
+
+        if(Input.GetKeyDown(KeyCode.X) && hasGrabbed){
+            ThrowObject();
+        }
+    }
+
+
+    private void ThrowObject(){
+        Vector2 input;
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+        
+        if(throwableObject != null){
+        throwableObject.GetComponent<Rigidbody2D>().velocity = transform.right * throwForce;
+        // Debug.DrawRay(transform.position, new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical")), Color.red);
+        }
+        hasGrabbed=false;
+
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position+Vector3.right*transform.localScale.x*distance);
+    }
+}
