6c0b2fc38e7698de2636e74a4cb957971bfeac8d
ltb <ltb@cin.ufpe.br>
2019-05-13 00:18:22 -0300

    add scriptable_object throwable

--- Assets/Throwable.cs
+++ Assets/Throwable.cs
@@ -4,17 +4,42 @@ using UnityEngine;
 
 public class Throwable : MonoBehaviour
 {
+    public ThrowableObject throwableObject;
     public bool active;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();
+        if(renderer){
+            renderer.sprite = throwableObject.sprite;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
         if(other.transform.tag == "Enemy" && active){
             other.gameObject.GetComponent<LivingComponent>().Damage(1);
         }
+
+        if(DestroyOnImpact() && active && other.transform.tag != "Char"){
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other) {
+        if(other.transform.tag == "Enemy" && active){
+            other.gameObject.GetComponent<LivingComponent>().Damage(1);
+        }
+
+        if(DestroyOnImpact() && active && other.transform.tag != "Char"){
+            Destroy(gameObject);
+        }
+    }
+
+    public bool DestroyOnImpact(){
+        if(!throwableObject.isImortal){
+            return true;
+        }
+        return false;
     }
 
     // Update is called once per frame
---
+++ Assets/ThrowableObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName="Throwable Object", menuName ="Throwable/List", order = 1)]
+public class ThrowableObject : ScriptableObject
+{
+    public string objectName;
+    public bool isImortal;
+    public Sprite sprite;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
